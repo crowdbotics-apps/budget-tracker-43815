6bf6e88be6015e710d24d98ac23b056744f0edcd
crowdbotics-apps <team@crowdbotics.com>
2023-10-07 08:45:34 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Items(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=255,blank=True,)
+    price = models.DecimalField(max_digits=5,decimal_places=2,blank=True,)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,)
