6714fbf6ba616db1244c1feb2182ee43c0d8ac22
Crowdbotics <team@crowdbotics.com>
2023-10-07 05:07:16 +0000

    Initial commit

---
+++ backend/budget_tracker_43815/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for budget_tracker_43815 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "budget_tracker_43815.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "budget-tracker-43815",
+  url: "https://budget-tracker-43815.botics.co",
+  api: "https://budget-tracker-43815.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
