4bd98d70efbd4e1047293e0f1f51cd8cc7702ff0
Saad bin Abid <saad.abid@crowdbotics.com>
2023-10-07 13:34:58 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "budget-tracker-43815",
   url: "https://budget-tracker-43815.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,64 +1,110 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import React from "react";
+import { View, Text, Button, FlatList, SafeAreaView, StyleSheet } from "react-native";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.group} />
-        <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
+  const users = [{
+    name: "User 1",
+    pendingAmount: "100"
+  }, {
+    name: "User 2",
+    pendingAmount: "200"
+  }, {
+    name: "User 3",
+    pendingAmount: "300"
+  }];
+  const expenses = [{
+    month: "January",
+    persons: "3",
+    cost: "300"
+  }, {
+    month: "February",
+    persons: "2",
+    cost: "200"
+  }, {
+    month: "March",
+    persons: "4",
+    cost: "400"
+  }];
+  return <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Total Cost: 1000</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Item List" onPress={() => {}} />
+        <Button title="Add User" onPress={() => {}} />
+      </View>
+      <FlatList data={users} renderItem={({
+      item
+    }) => <View style={styles.card}>
+            <Text style={styles.cardText}>{item.name}</Text>
+            <Text style={styles.cardText}>
+              Pending Amount: {item.pendingAmount}
+            </Text>
+          </View>} keyExtractor={item => item.name} />
+      <View style={styles.table}>
+        <View style={styles.row}>
+          <Text style={styles.tableHeader}>Month</Text>
+          <Text style={styles.tableHeader}>Persons</Text>
+          <Text style={styles.tableHeader}>Cost</Text>
         </View>
-        <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+        {expenses.map(expense => <View style={styles.row} key={expense.month}>
+            <Text style={styles.cell}>{expense.month}</Text>
+            <Text style={styles.cell}>{expense.persons}</Text>
+            <Text style={styles.cell}>{expense.cost}</Text>
+          </View>)}
+      </View>
+    </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F8F8FC",
-    height: "100%"
-  },
-  scrollView: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20
+    padding: 10,
+    backgroundColor: "#F8F8FC"
   },
-  group: {
-    alignItems: "center"
-  },
-  logo: {
-    height: 180,
-    width: 180,
-    padding: 40,
-    borderRadius: 30,
-    margin: 40
-  },
-  text: {
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
     textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
+    marginVertical: 20
   },
-  footer: {
-    textAlign: "center",
-    fontSize: 18,
-    color: "#828AB0",
-    fontWeight: 700,
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginBottom: 20
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  cardText: {
+    fontSize: 18,
+    color: "#333"
+  },
+  table: {
+    marginTop: 20
+  },
+  tableHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+    flex: 1
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc"
+  },
+  cell: {
+    fontSize: 18,
+    color: "#333",
+    flex: 1,
+    textAlign: "center"
   }
-})
-
-export default WelcomeScreen
+});
+export default WelcomeScreen;
