b9bc2b42650bc6e5a9f327936d7f69f9d1f13340
Crowdbotics <engineering@crowdbotics.com>
2023-11-15 17:31:13 +0000

    Update store.

--- store/budgettrackerAPI/api.js
+++ store/budgettrackerAPI/api.js
@@ -1,108 +1,79 @@
-import axios from "axios";
+import axios from "axios"
 const budgettrackerAPI = axios.create({
   baseURL: "https://budget-tracker-43815.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return budgettrackerAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_items_list(payload) {
-  return budgettrackerAPI.get(`/api/v1/items/`);
+  return budgettrackerAPI.get(`/api/v1/items/`)
 }
-
 function api_v1_items_create(payload) {
-  return budgettrackerAPI.post(`/api/v1/items/`, payload);
+  return budgettrackerAPI.post(`/api/v1/items/`, payload)
 }
-
 function api_v1_items_retrieve(payload) {
-  return budgettrackerAPI.get(`/api/v1/items/${payload.id}/`);
+  return budgettrackerAPI.get(`/api/v1/items/${payload.id}/`)
 }
-
 function api_v1_items_update(payload) {
-  return budgettrackerAPI.put(`/api/v1/items/${payload.id}/`, payload);
+  return budgettrackerAPI.put(`/api/v1/items/${payload.id}/`, payload)
 }
-
 function api_v1_items_partial_update(payload) {
-  return budgettrackerAPI.patch(`/api/v1/items/${payload.id}/`, payload);
+  return budgettrackerAPI.patch(`/api/v1/items/${payload.id}/`, payload)
 }
-
 function api_v1_items_destroy(payload) {
-  return budgettrackerAPI.delete(`/api/v1/items/${payload.id}/`);
+  return budgettrackerAPI.delete(`/api/v1/items/${payload.id}/`)
 }
-
 function api_v1_login_create(payload) {
-  return budgettrackerAPI.post(`/api/v1/login/`, payload);
+  return budgettrackerAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return budgettrackerAPI.post(`/api/v1/signup/`, payload);
+  return budgettrackerAPI.post(`/api/v1/signup/`, payload)
 }
-
 function api_v1_users_list(payload) {
-  return budgettrackerAPI.get(`/api/v1/users/`);
+  return budgettrackerAPI.get(`/api/v1/users/`)
 }
-
 function api_v1_users_retrieve(payload) {
-  return budgettrackerAPI.get(`/api/v1/users/${payload.id}/`);
+  return budgettrackerAPI.get(`/api/v1/users/${payload.id}/`)
 }
-
 function api_v1_users_destroy(payload) {
-  return budgettrackerAPI.delete(`/api/v1/users/${payload.id}/`);
+  return budgettrackerAPI.delete(`/api/v1/users/${payload.id}/`)
 }
-
 function rest_auth_login_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/login/`, payload);
+  return budgettrackerAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/logout/`);
+  return budgettrackerAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/password/change/`, payload);
+  return budgettrackerAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/password/reset/`, payload);
+  return budgettrackerAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return budgettrackerAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/registration/`, payload);
+  return budgettrackerAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return budgettrackerAPI.post(`/rest-auth/registration/resend-email/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return budgettrackerAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return budgettrackerAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return budgettrackerAPI.get(`/rest-auth/user/`);
+  return budgettrackerAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return budgettrackerAPI.put(`/rest-auth/user/`, payload);
+  return budgettrackerAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return budgettrackerAPI.patch(`/rest-auth/user/`, payload);
+  return budgettrackerAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_items_list,
@@ -127,4 +98,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
